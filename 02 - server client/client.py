41bdb7d1891dbdf8b9cb70d30d2cd06eb161f273
clemens.kruckel - laptop <clemens.kruckel@ugent.be>
2022-06-04 19:48:58 +0200

    server client communication added

--- 02 - server client/client.py
+++ 02 - server client/client.py
@@ -1,20 +1,35 @@
 
 import time
-
 import socket
 
+#use header to announce how long the message will be
+HEADERSIZE = 10
+
+#establish connection with server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(),1234))
 #s.connect(('192.168.126.9',1234))
 
-msg_full = ''
 while True:
-    msg = s.recv(8)
-    msg_full += msg.decode("utf-8")
-    print(msg.decode("utf-8"))
-    print(len(msg))
-    if len(msg) < 8:
-        break
+
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print(f'new message length: {msg[:HEADERSIZE]}')
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
+
+        full_msg += msg.decode("utf-8")
+        print(msg.decode("utf-8"))
+        print(len(msg))
+
+        if len(full_msg)-HEADERSIZE == msglen:
+            print("full msg recvd")
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = ''
 
 print(msg_full)
 
--- 02 - server client/server.py
+++ 02 - server client/server.py
@@ -2,22 +2,30 @@
 import time
 import socket
 
+#use header to announce how long the message will be
+HEADERSIZE = 10
+
+#establish connection with client
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print(socket.gethostname())
 s.bind((socket.gethostname(),1234))
 s.listen(5)
 
+
+
 while True:
     print('test')
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Welcome to the server", "utf-8"))
+
+    msg = "Welcome to the server"
+    msg = f'{len(msg):<{HEADERSIZE}}' + msg #left align
+
+    clientsocket.send(bytes(msg, "utf-8"))
     time.sleep(1)
 
     #msg = clientsocket.recv(1024)
     #print(msg.decode("utf-8"))
-
-
 
 
 #for i in range(10):
